8a131924d7f180644c5df99e8e76998b15b2d184
palmtopj <palmtopj@uw.edu>
2023-06-20 23:34:34 -0700

    set seattle default location

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-const apiKeys = {f816da443ddeb826d825bca09d64b3c4};
-const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKeys}`;
+const apiKey = "f816da443ddeb826d825bca09d64b3c4";
+const city = "Seattle";
+const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
+console.log(city);
