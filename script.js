61c1a45bfc56dee0f471c6a1b5f0ae587df89ac8
palmtopj <palmtopj@uw.edu>
2023-06-20 23:58:57 -0700

    const city works

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const apiKey = "f816da443ddeb826d825bca09d64b3c4";
-const city = "Seattle";
+const city = "Miami";
 const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-console.log(city);
+fetch(api)
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data)
+        const temperature = data.main.temp;
+        const description = data.weather[0].description;
+        const location = data.name;
+
+        document.getElementById("location").innerHTML = location;
+        document.getElementById("temperature").innerHTML = temperature;
+        document.getElementById("description").innerHTML = description;
+    });
