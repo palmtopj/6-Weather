393adfa3a377a2bd29e67af14ce2689caf977739
palmtopj <palmtopj@uw.edu>
2023-06-22 05:10:22 -0700

    added search

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+function getWeather (city) {
 const apiKey = "f816da443ddeb826d825bca09d64b3c4";
-const city = "Seattle";
+// const city = "Seattle";
 const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
 fetch(api)
@@ -17,3 +18,13 @@ fetch(api)
         document.getElementById("temperature").innerHTML = temperature;
         document.getElementById("description").innerHTML = description;
     });
+
+  }
+
+function setQuery(event) {
+    if (event.key === "Enter") {
+        const city = event.target.value.trim();
+        getWeather(city);
+        event.target.value = "";
+    }
+}
